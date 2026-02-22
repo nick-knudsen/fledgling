5f3d9257e4277e7f5542de886419f2b04da7fcd8
nick-knudsen <45007458+nick-knudsen@users.noreply.github.com>
2026-02-22 15:49:10 -0500

    Adding beta testers

--- api.py
+++ api.py
@@ -24,7 +24,7 @@ EBIRD_API_KEY = os.environ["EBIRD_API_KEY"]
 
 app = FastAPI(title="Fledgling")
 
-BETA_USERS = {"gavin", "lachlan", "christian", "sylvia", "laura", "nick", "carter", "jayce", "cam", "alex", "jacob", "brady", "eric", "brian", "jack", "jackson", "taran", "anna", "garrett", "john", "miles", "metasilk", "nate"}
+BETA_USERS = {"gavin", "lachlan", "christian", "sylvia", "laura", "nick", "carter", "jayce", "cam", "alex", "jacob", "brady", "eric", "brian", "jack", "jackson", "taran", "anna", "garrett", "john", "miles", "metasilk", "nate", "erin"}
 
 
 @app.get("/api/beta-check")
@@ -415,3 +415,4 @@ def get_hotspot_details(req: HotspotDetailRequest):
 
 # Serve the frontend
 app.mount("/", StaticFiles(directory=str(BASE_DIR / "static"), html=True), name="static")
+
