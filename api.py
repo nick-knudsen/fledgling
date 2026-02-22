312535ea6562a38d85d0b9a8e5bbc288d75f0a48
Nick Knudsen <nick.knudsen14@gmail.com>
2026-02-22 00:28:28 -0500

    adding beta testers

--- api.py
+++ api.py
@@ -24,7 +24,7 @@ EBIRD_API_KEY = os.environ["EBIRD_API_KEY"]
 
 app = FastAPI(title="Fledgling")
 
-BETA_USERS = {"gavin", "lachlan", "christian", "sylvia", "laura", "nick", "carter", "jayce", "cam", "alex", "jacob", "brady", "eric", "brian", "jack", "jackson", "taran", "anna", "garrett", "john"}
+BETA_USERS = {"gavin", "lachlan", "christian", "sylvia", "laura", "nick", "carter", "jayce", "cam", "alex", "jacob", "brady", "eric", "brian", "jack", "jackson", "taran", "anna", "garrett", "john", "miles"}
 
 
 @app.get("/api/beta-check")
